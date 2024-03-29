191eb89af3688d87e5176a6816a331081768d3fc
phucndb <phucndb@inspirelab.io>
2024-03-29 11:21:13 +0700

    update bundle

---
+++ Bundle/web-mobile/Cocopark/index.60f38.js
@@ -0,0 +1,20 @@
+System.register("bundle://Cocopark/_virtual/Cocopark",[],(function(){return{execute:function(){}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/Cocopark', 'bundle://Cocopark/_virtual/Cocopark'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
