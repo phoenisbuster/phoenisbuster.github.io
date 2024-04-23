202c8b167a6b4aa1c5c67f2194eb440d0c9f16d2
phucndb <phucndb@inspirelab.io>
2024-04-23 16:47:51 +0700

    Update firebaseHelper.js

--- firebaseHelper.js
+++ firebaseHelper.js
@@ -79,136 +79,32 @@ window.signInWithTwitterPopup = function()
     });
 } 
 
-//FCM -------------------------------------------------
-// firebaseMessaging = getMessaging(app);
+window.signInWithTwitterCred = function()
+{
+  return signInWithCredential(auth, credential)
+    .then((result) => 
+    {
+      // Signed in 
+      const credential = TwitterAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const secret = credential.secret;
 
-// if ("serviceWorker" in navigator) {
-//   navigator.serviceWorker
-//     .register("./firebase-messaging-sw.js")
-//     .then(function (registration) {
-//       console.log("Registration successful");
-//       var serviceWorker;
-//       if (registration.installing) {
-//         serviceWorker = registration.installing;
-//       } else if (registration.waiting) {
-//         serviceWorker = registration.waiting;
-//       } else if (registration.active) {
-//         serviceWorker = registration.active;
-//       }
-
-//       if (serviceWorker) {
-//         // swRegistration = serviceWorker;
-//         console.log("sw current state", serviceWorker.state);
-//         if (serviceWorker.state == "activated") {
-//           //If push subscription wasnt done yet have to do here
-//           console.log("sw already activated - Do watever needed here");
-//           // subscribeUser();
-//         }
-//         serviceWorker.addEventListener("statechange", function (e) {
-//           console.log("sw statechange : ", e.target.state);
-//           if (e.target.state == "activated") {
-//             // use pushManger for subscribing here.
-//             console.log(
-//               "Just now activated. now we can subscribe for push notification"
-//             );
-//             // subscribeUser();
-//           }
-//         });
-//       }
-//     })
-//     .catch(function (err) {
-//       console.log("Service worker registration failed, error:", err);
-//     });
-// }
-
-// // Use serviceWorker.ready to ensure that you can subscribe for push
-// navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
-//   swRegistration = serviceWorkerRegistration;
-//   const options = {
-//     userVisibleOnly: true,
-//     applicationServerKey: publicVapidKey,
-//   };
-//   serviceWorkerRegistration.pushManager.subscribe(options).then(
-//     (pushSubscription) => {
-//       // The push subscription details needed by the application
-//       // server are now available, and can be sent to it using,
-//       // for example, an XMLHttpRequest.
-//     },
-//     (error) => {
-//       // During development it often helps to log errors to the
-//       // console. In a production environment it might make sense to
-//       // also report information about errors back to the
-//       // application server.
-//       console.error(error);
-//     }
-//   );
-// });
-
-// navigator.serviceWorker.addEventListener("message", (event) => {
-//   console.log("zzz Received a message from service worker");
-//   if (event.data.message == "push_message_received") {
-//     fcmMessage = event.data.data;
-//   }
-// });
-
-// function subscribeUserOnClick() {
-//   console.log("zzz subscribeUserOnClick");
-//   subscribeUser();
-//   let canvas = document.getElementById("GameCanvas");
-//   if (fcmToken == null) return;
-//   if (canvas != null) {
-//     canvas.removeEventListener("touchstart", subscribeUserOnClick);
-//     canvas.removeEventListener("click", subscribeUserOnClick);
-//   }
-// }
-
-// let canvas = document.getElementById("GameCanvas");
-// if (canvas != null) {
-//   canvas.addEventListener("touchstart", subscribeUserOnClick);
-//   canvas.addEventListener("click", subscribeUserOnClick);
-// }
-
-// function subscribeUser() {
-//   Notification.requestPermission().then((permission) => {
-//     if (permission === "granted") {
-//       console.log("zzzzz Notification permission granted.");
-//       getFCMToken();
-//     } else {
-//       console.error("zzzzz Notification permission denied.");
-//     }
-//   });
-// }
-
-// async function getFCMToken() {
-//   await swRegistration;
-
-//   console.log("zzzz", getToken);
-//   getToken(firebaseMessaging, {
-//     vapidKey: publicVapidKey,
-//     serviceWorkerRegistration: swRegistration,
-//   })
-//     .then((currentToken) => {
-//       if (currentToken) {
-//         console.log("zzzzz got Token");
-//         fcmToken = currentToken;
-//       } else {
-//         console.log("zzzzz No registration token available.");
-//       }
-//     })
-//     .catch((error) => {
-//       console.log(
-//         "zzzzz Error getting token:",
-//         error,
-//         "getTokenTryCount",
-//         getTokenTryCount
-//       );
-//       if (getTokenTryCount > 0) {
-//         getFCMToken();
-//         getTokenTryCount--;
-//       }
-//     });
-// }
-//End FCM -------------------------------------------------
+      console.log("Sign In Twitter", credential, token, secret);
+      return {token: token, secret: secret, error: false};
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = TwitterAuthProvider.credentialFromError(error);
+      // ...
+      console.error("Sign In Twitter Error", errorCode, errorMessage, email, credential);
+      return {token: "", secret: "", error: true};
+    });
+}
 
 //FirebaseAnalytics -------------------------------------------------
 window.logEvent = function(event_name, param1, param2) {
