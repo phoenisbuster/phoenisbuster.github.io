d314f923258dd5e8997c842432524dd048583086
phucndb <phucndb@inspirelab.io>
2024-05-20 16:17:29 +0700

    Update wagmiHelper.js

--- wagmiHelper.js
+++ wagmiHelper.js
@@ -1,4 +1,38 @@
 import React from 'https://esm.sh/react@18.2.0'
 import { QueryClient } from 'https://esm.sh/@tanstack/react-query'
 import { createClient } from 'https://esm.sh/viem@2.x'
-import { createConfig } from 'https://esm.sh/wagmi'
+import { 
+    createConfig, 
+    createStorage, 
+    useAccount ,
+    useBalance
+} from 'https://esm.sh/wagmi'
+import { mainnet, sepolia } from 'https://esm.sh/wagmi/chains'
+import { metaMask } from 'https://esm.sh/wagmi/connectors'
+
+const wagmiStorage = createStorage({ storage: localStorage })
+
+const wagmiConfig = createConfig({
+    chains: [arbitrumSepolia],
+    connectors: [metaMask()],
+    storage: wagmiStorage,
+    transports: {
+      [arbitrumSepolia.id]: http(),
+    },
+})
+
+window.wagmiAccounts = useAccount({
+    wagmiConfig,
+})
+
+window.wagmiBalance = function() {
+    return useBalance({
+        address: '0x4557B18E779944BFE9d78A672452331C186a9f48',
+        wagmiConfig,
+        unit: 'ether',
+    })
+}
+
+window.wagmiChainId = useChainId({
+    wagmiConfig,
+})
