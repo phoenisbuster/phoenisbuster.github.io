a3c305ae451936b76ea6735810d8e411b7388927
phucndb <phucndb@inspirelab.io>
2024-05-20 15:50:19 +0700

    Create wagmiHelper.js

---
+++ wagmiHelper.js
@@ -0,0 +1,4 @@
+import React from 'https://esm.sh/react@18.2.0'
+import { QueryClient } from 'https://esm.sh/@tanstack/react-query'
+import { createClient } from 'https://esm.sh/viem@2.x'
+import { createConfig } from 'https://esm.sh/wagmi'
